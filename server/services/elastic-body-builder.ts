fe869c059733d655f6ca92051d132acbc0368cdb
omer <omerg206@gmail.com>
2021-02-18 00:09:11 +0200

    free text filter, size work

--- server/services/elastic-body-builder.ts
+++ server/services/elastic-body-builder.ts
@@ -4,8 +4,25 @@ import bodybuilder from 'bodybuilder';
 
 
 
-export const createGetTableDataFilterElasticQuery = (filters?: GetTableDateFilters): any => {
-    return bodybuilder().query('multi_match', 'query', "Vickie", { type: "phrase_prefix" })
-        .build();
+export const createGetTableDataFilterElasticQuery = (filters: GetTableDateFilters): any => {
+    let filterQuery = bodybuilder().size(filters.pageSize || 10);
 
+
+    if (filters.textFilter) {
+        filterQuery = filterQuery.query('multi_match', 'query', filters.textFilter, {
+            "fields": ["test"], "operator": "and", "analyzer": "my_analyzer"
+        })
+    }
+
+    if (filters.sortFieldName) {
+        filterQuery = filterQuery.sort('email', filters.sortOrder || 'desc')
+    }
+
+    if (filters.dateFilter) {
+        filterQuery.query("range", "date", { lte: filters.dateFilter })
+    }
+
+
+
+    return filterQuery.build();
 }
--- server/services/elastic.ts
+++ server/services/elastic.ts
@@ -1,11 +1,11 @@
 import { GetTableDateFilters } from './../../shared/table-data.type';
-import elasticsearch from 'elasticsearch';
+import elasticsearch, { SearchResponse } from 'elasticsearch';
 import { Request, Response } from 'express';
 import fs from 'fs';
 import path from 'path';
 import { TableData } from '../../shared/table-data.type';
 import { createGetTableDataFilterElasticQuery } from './elastic-body-builder';
-
+import { reduce } from 'lodash';
 const index = "table-data"
 const client = new elasticsearch.Client({
     host: 'localhost:9200',
@@ -14,8 +14,9 @@ const client = new elasticsearch.Client({
 });
 
 
-export const getTableData = (req: Request, res: Response) => {
+export const getTableData = async (req: Request, res: Response) => {
     const queryParams = req.query;
+
 
     const sortParams: GetTableDateFilters = {
         dateFilter: queryParams?.dateFilter as any,
@@ -24,57 +25,42 @@ export const getTableData = (req: Request, res: Response) => {
         textFilter: queryParams?.textFilter as string,
         pageNumber: queryParams?.pageNumber as any,
         pageSize: queryParams?.pageSize as any
-
     }
 
-    return res.status(200).json({ payload: [] });
+    // const sortParamsNonNull: Partial<GetTableDateFilters> = reduce(sortParams, (acc: Partial<GetTableDateFilters>, value: any, key: any) => {
+    //     if (sortParams[key]) {
+    //         acc[key] = value;
+    //     }
+
+    //     return acc;
+    // }, {});
+
+    try {
+        const { hits } = await getDataFromElastic(sortParams);
+        return res.status(200).json({ payload: hits, total: hits.total });
+    } catch (e) {
+        return res.status(503).json({ error: `elastic error ${e}` });
+    }
+
+
+
 }
 
 
 
-const getDataFromElastic = async () => {
+const getDataFromElastic = async (sortFilters: GetTableDateFilters): Promise<elasticsearch.SearchResponse<unknown>> => {
     try {
-        const query: any = createGetTableDataFilterElasticQuery();
-        const { hits: res } = await client.search({
-            index, body: { ...query }
-        });
-
+        const query: any = createGetTableDataFilterElasticQuery(sortFilters);
+        return await client.search({ index, body: query });
     } catch (e) {
         console.error(e);
-
+        throw e
     }
-
-
 }
 
 export const insertMockToElastic = async () => {
-
     try {
-        const isIndexExists = await client.indices.exists({ index });
-
-        if (isIndexExists) {
-
-            client.indices.delete({ index });
-        }
-
-
-        const rawSetting = fs.readFileSync(path.join(__dirname, "../utils/settings.json"));
-        const parsedSetting: any[] = JSON.parse(rawSetting.toString());
-
-        const rawMapping = fs.readFileSync(path.join(__dirname, "../utils/mapping.json"));
-        const parsedMapping: any[] = JSON.parse(rawMapping.toString());
-
-        await client.indices.create({
-            index, body: {
-                settings: parsedSetting,
-                mappings: parsedMapping
-            }
-        });
-
-
-
-
-
+        await createIndex();
         const isIndexEmpty = await client.indices.stats({ index }).then((res) => res.indices[index].total.docs.count === 0);
 
         if (isIndexEmpty) {
@@ -93,9 +79,35 @@ export const insertMockToElastic = async () => {
             });
             console.log(res);
         }
-        getDataFromElastic();
+
     } catch (e) {
         console.error(e);
     }
 
+}
+
+const deleteIndex = async () => {
+    const isIndexExists = await client.indices.exists({ index });
+
+    if (isIndexExists) {
+        client.indices.delete({ index });
+    }
+}
+const createIndex = async () => {
+    const isIndexExists = await client.indices.exists({ index });
+
+    if (!isIndexExists) {
+        const rawSetting = fs.readFileSync(path.join(__dirname, "../utils/settings.json"));
+        const parsedSetting: any[] = JSON.parse(rawSetting.toString());
+
+        const rawMapping = fs.readFileSync(path.join(__dirname, "../utils/mapping.json"));
+        const parsedMapping: any[] = JSON.parse(rawMapping.toString());
+
+        await client.indices.create({
+            index, body: {
+                settings: parsedSetting,
+                mappings: parsedMapping
+            }
+        });
+    }
 }
